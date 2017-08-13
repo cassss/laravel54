d226c439095363c176863bb5e57aa3524901de5d
cassss <18663006337@163.com>
2017-08-14 00:06:16 +0800

    main

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -36,7 +36,7 @@
                 <div class="blog-post">
                     <h2 class="blog-post-title"><a href="/posts/{{$post->id}}" >{{$post->title}}</a></h2>
                     <p class="blog-post-meta">{{$post->created_at->toFormattedDateString()}}<a href="/user/5">{{$post->user->name}}</a></p>
-                    <p>{!!strip_tags(str_limit($post->content,100,'...'))!!}
+                    <p>{!!str_limit(strip_tags($post->content),100,'...')!!}
                     <p class="blog-post-meta">赞 {{$post->zans_count}}  | 评论 {{$post->comments_count}}</p>
                 </div>
             @endforeach
